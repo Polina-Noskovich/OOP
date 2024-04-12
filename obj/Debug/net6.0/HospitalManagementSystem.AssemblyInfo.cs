158819cee2742798954923058dc58a88f2893ee4
Polina <noskovicpolina@gmail.com>
2024-04-12 23:38:48 +0300

    HMS Version 1.0

--- obj/Debug/net6.0/HospitalManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net6.0/HospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08299064d39fafcf1c07aac094a0cde07d8f7451")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
